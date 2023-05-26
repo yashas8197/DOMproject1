9515d4dff21da35d67c010681647d4e116f73c2e
yashas <yashasjr7@gmail.com>
2023-05-26 12:52:52 +0530

    added quotes

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ console.log(quote)
 let person = document.querySelector('.person');
 console.log(person)
 
+const quotes = [{
+    quote:  "I’m not a great programmer; I’m just a good programmer with great habits.",
+    person: "Kent Beck."
+ 
+ },{quote: "Good programmers know what to write. Great ones know what to rewrite and reuse" ,
+ person: "Eric S. Raymond."
+ 
+ },{quote: "Computer science education cannot make anybody an expert programmer any more than studying brushes and pigment can make somebody an expert painter.",
+ person: "Eric S. Raymond."
+ 
+ },{quote: "Software and cathedrals are much the same; first we build them, then we pray." ,
+ person: "Anonymous."
+ 
+ },{quote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+ person: "Martin Fowler"
+ 
+ },{quote: "Real programmers can write assembly code in any language.",
+ person: "Larry Wall"
+ 
+ },{quote: "Good programmers use their brains, but good guidelines save us having to think out every case." ,
+ person: "Francis Glassborow."
+ 
+ },{quote: "Documentation is the castor oil of programming. Managers think it is good for programmers and programmers hate it." ,
+ person: "Gerald Weinberg."
+ 
+ },{quote: "Common programmer thought pattern: there are only three numbers: 0, 1, and n.",
+ person: "Joel Spolsky."
+ 
+ },]
