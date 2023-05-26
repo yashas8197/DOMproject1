1632caaac2655019497795b3294ad86ce5de4ef5
yashas <yashasjr7@gmail.com>
2023-05-26 12:51:46 +0530

    added variable

---
+++ script.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("#new-quote");
+console.log(btn)
+let quote = document.getElementsByTagName("span");
+console.log(quote)
+let person = document.querySelector('.person');
+console.log(person)
+
